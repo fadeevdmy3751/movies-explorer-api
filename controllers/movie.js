da495777aba9c5a36ad4a08ea1775d7c417feb10
= <=>
2022-12-06 14:49:40 +0300

    movieId -> _id

--- controllers/movie.js
+++ controllers/movie.js
@@ -54,7 +54,7 @@ function createMovie(req, res, next) {
         return;
       } // already liked
       movieModel.create({ ...body, owner: req.user._id })
-        .then((newMovie) => res.send({ message: `фильм ${newMovie.movieId} добавлен` }))
+        .then((newMovie) => res.send({ message: `фильм ${newMovie._id} добавлен` }))
         .catch((err) => {
           if (err.name === 'ValidationError') {
             next(new IncorrectDataError(`параметры фильма: ${Object.keys(err.errors).join()}`));
