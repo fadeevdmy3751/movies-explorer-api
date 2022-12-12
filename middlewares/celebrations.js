f993a5dc84b1da6b9932a48f60afb518da58d4fb
fadeevdmy3751 <fadeevdmy7991@gmail.com>
2022-12-12 17:48:15 +0300

    email validation tuning

--- middlewares/celebrations.js
+++ middlewares/celebrations.js
@@ -3,20 +3,20 @@ const { celebrate, Joi } = require('celebrate');
 const validateProfile = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email({ tlds: { allow: false } }),
   }),
 });
 
 const validateNewUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email({ tlds: { allow: false } }),
     password: Joi.string().required(),
   }),
 });
 const validateCredentials = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email({ tlds: { allow: false } }),
     password: Joi.string().required(),
   }),
 });
